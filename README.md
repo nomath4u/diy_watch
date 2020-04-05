978e7ab1a63fee07bd8dc437e75ec3c9b3f729c7
Chris Harper <chris.harper@apricitycode.com>
2020-04-04 18:44:00 -0700

    Screen test code
    Enough to make sure the screen turns on and the api is working well.
    Also test that the backlight PWM is working

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Beginnings of personal watch project
+
+Libraries
+=========
+
+1. Adafruit GFX library
+2. Adafruit ST7735 library
+3. Adafruit\_ZeroDMA
--- main.ino
+++ main.ino
@@ -3,14 +3,38 @@
  ****/
 
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <SPI.h>
+
+/********************
+  Screen pins
+********************/
+//Using hardware SPI
+#define TFT_CS 7
+#define TFT_RST -1 // Tied directly to arduino RST
+#define TFT_DC 15
+#define LITE A0
+/*******************
+  End Screen pins
+******************/
+
+/******************
+  Screen object
+******************/
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 void setup(){
-  Scheduler.startLoop(blink);
+
+    setup_screen();
+    Scheduler.startLoop(blink);
+    Scheduler.startLoop(brightness);
 }
 
 
 void loop(){
     //Do nothing since we are scheduling
+    testdrawtext("Hello world!", ST77XX_WHITE);
 }
 
 void blink(){
@@ -18,3 +42,21 @@ void blink(){
     delay(500);
 }
 
+void setup_screen(){
+    tft.initR(INITR_BLACKTAB);    
+    tft.fillScreen(ST77XX_BLACK);
+
+}
+
+void brightness(){
+    static uint8_t backlight = 255;
+    analogWrite(LITE, backlight);
+    backlight--;
+}
+
+void testdrawtext(char *text, uint16_t color){
+    tft.setCursor(0,0);
+    tft.setTextColor(color);
+    tft.setTextWrap(true);
+    tft.print(text);
+}
